60f7c9d21e523e80dd95bb902d33c2725a124347
Kallyan01 <singhakallyan@gmail.com>
2021-04-16 01:28:35 +0530

    added style to navigation menu and added req. js

--- Medhelp/script.js
+++ Medhelp/script.js
@@ -1,3 +1,13 @@
-
-alert("Hi i am Medhelp");
-console.log("welcome to console")
+function bmenu()
+{
+    let nbar = document.getElementById('nbar');
+    if(nbar.style.height !="500px")
+    {
+    nbar.style.height="500px";
+    nbar.style.transition="0.8s ease-in";
+    }
+    else
+    {
+    nbar.style.height="60px";
+    }
+}
