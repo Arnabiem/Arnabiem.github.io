25346049824e583b46c61baf7d4cd38c5a7c2e5e
SayanDeveloper <samantasayan23@gmail.com>
2021-04-20 20:54:07 +0530

    navbar bug fixed (Sayan)

--- Medhelp/script.js
+++ Medhelp/script.js
@@ -1,13 +1,28 @@
+var hei = 0;
 function bmenu()
 {
-    let nbar = document.getElementById('nbar');
-    if(nbar.style.height !="500px")
-    {
-    nbar.style.height="500px";
-    nbar.style.transition="0.4s ease-in";
+    if (window.innerWidth <= 760) {
+        let nbar = document.getElementById('nbar');
+        if(nbar.style.height !="500px")
+        {
+            hei = 500;
+            nbar.style.height="500px";
+            nbar.style.transition="0.4s ease-in";
+        }
+        else
+        {
+            hei = 60;
+            nbar.style.height="60px";
+        }
     }
-    else
-    {
-    nbar.style.height="60px";
+}
+
+window.onresize = function() {
+    let nbar = document.getElementById('nbar');
+    if (window.innerWidth > 760) {
+        nbar.style.height = "258px";
+    }
+    else {
+        nbar.style.height = hei + "px";
     }
 }
