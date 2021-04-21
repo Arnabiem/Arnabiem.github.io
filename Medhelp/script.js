7332924745f68830e2c2e917dc1f01d648afd3a2
SayanDeveloper <samantasayan23@gmail.com>
2021-04-21 19:24:34 +0530

    removed my codes (Sayan)

--- Medhelp/script.js
+++ Medhelp/script.js
@@ -1,28 +1,13 @@
-var hei = 0;
 function bmenu()
 {
-    if (window.innerWidth <= 760) {
-        let nbar = document.getElementById('nbar');
-        if(nbar.style.height !="500px")
-        {
-            hei = 500;
-            nbar.style.height="500px";
-            nbar.style.transition="0.4s ease-in";
-        }
-        else
-        {
-            hei = 60;
-            nbar.style.height="60px";
-        }
-    }
-}
-
-window.onresize = function() {
     let nbar = document.getElementById('nbar');
-    if (window.innerWidth > 760) {
-        nbar.style.height = "258px";
+    if(nbar.style.height !="500px")
+    {
+    nbar.style.height="500px";
+    nbar.style.transition="0.4s ease-in";
     }
-    else {
-        nbar.style.height = hei + "px";
+    else
+    {
+    nbar.style.height="60px";
     }
 }
